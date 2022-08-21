fe797a6faff5a357e2a1b24896aac6f989ce1526
liliia87 <lilia.krivelyova@gmail.com>
2022-08-20 22:39:06 -0700

    put list with all engineers and machines on splash page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      return View(_db.EngineerMachine.ToList());
     }
+
   }
 }
